6b939ebb981e058dd0e93ab818784de56338eb56
Chen Zhen-Hua <shizsun0609tw@gmail.com>
2021-04-30 11:09:28 +0800

    complete timewraper

--- src/simulation/kinematics.cpp
+++ src/simulation/kinematics.cpp
@@ -7,14 +7,6 @@
 
 namespace kinematics {
 void forwardSolver(const acclaim::Posture& posture, acclaim::Bone* bone) {
-    // TODO
-    // This function will be called with bone == root bone of the skeleton
-    // You should set these variables:
-    //     bone->start_position = Eigen::Vector4d::Zero();
-    //     bone->end_position = Eigen::Vector4d::Zero();
-    //     bone->rotation = Eigen::Matrix4d::Zero();
-    // The sample above just set everything to zero
-    
     while (bone != NULL) {
         bone->rotation = bone->parent ? 
             Eigen::Affine3d(bone->parent->rotation * bone->rot_parent_current * util::rotateDegreeZYX(posture.bone_rotations[bone->idx])) : 
@@ -31,18 +23,27 @@ void forwardSolver(const acclaim::Posture& posture, acclaim::Bone* bone) {
     }
 }
 
+#define PI 3.1415926
 std::vector<acclaim::Posture> timeWarper(const std::vector<acclaim::Posture>& postures, int keyframe_old,
                                          int keyframe_new) {
     int total_frames = static_cast<int>(postures.size());
     int total_bones = static_cast<int>(postures[0].bone_rotations.size());
     std::vector<acclaim::Posture> new_postures = postures;
+
     for (int i = 0; i < total_frames; ++i) {
         for (int j = 0; j < total_bones; ++j) {
-            // TODO
-            // You should set these variables:
-            //     new_postures[i].bone_translations[j] = postures[i].bone_translations[j];
-            //     new_postures[i].bone_rotations[j] = postures[i].bone_rotations[j];
-            // The sample above just change nothing
+            float weights = float(abs(i - keyframe_new)) / total_frames;
+            int target_frame = i * keyframe_old / keyframe_new;
+
+            target_frame = target_frame > total_frames - 1 ? total_frames - 1 : target_frame;
+            
+            new_postures[i].bone_translations[j] = weights * postures[i].bone_translations[j] + (1 - weights) * postures[target_frame].bone_translations[j];
+            
+            Eigen::Quaternion temp_q = util::rotateDegreeXYZ(postures[i].bone_rotations[j])
+                                            .slerp(1.0f - weights, util::rotateDegreeXYZ(postures[target_frame].bone_rotations[j]));
+            Eigen::Vector3d temp_v = temp_q.toRotationMatrix().eulerAngles(0, 1, 2) * (180.0f / PI);
+            
+            new_postures[i].bone_rotations[j] = Eigen::Vector4d(temp_v[0], temp_v[1], temp_v[2], 0);
         }
     }
     return new_postures;
